3e2d1aca22bd8dcb965b4b47a18c5b726a0ceb1a
aniarya82 <hiten.iitkgp@gmail.com>
2017-07-28 10:33:11 +0530

    AnnotationEditor plugin initialized

---
+++ plugins/AnnotationEditor/js/TrackConfigTransformer.js
@@ -0,0 +1,55 @@
+/**
+ *  TrackConfigTransformer takes JBrowse track.config object and, if needed, 
+ *      modifies in place to use WebApollo-specific track types, etc.
+ */
+define( [ 'dojo/_base/declare' ],
+        function( declare ) {
+
+return declare( null, {
+
+
+constructor: function( args )  {
+   
+    this.transformers=[];
+    var browser=args.browser;
+    this.overridePlugins=browser.config.overridePlugins;
+    this.transformers["JBrowse/View/Track/HTMLFeatures"] = function(trackConfig) {
+        trackConfig.type = "AnnotationEditor/View/Track/DraggableHTMLFeatures";
+    };
+
+    // this.transformers["JBrowse/View/Track/CanvasFeatures"] = function(trackConfig) {
+    //     trackConfig.type = "AnnotationEditor/View/Track/WebApolloCanvasFeatures";
+    // };
+
+    // this.transformers["JBrowse/View/Track/Sequence"] = function(trackConfig) {
+    //     trackConfig.type = "AnnotationEditor/View/Track/AnnotSequenceTrack";
+    //     trackConfig.storeClass = "AnnotationEditor/Store/SeqFeature/ScratchPad";
+    //     trackConfig.style = { className: "{type}", 
+    //                           uniqueIdField : "id" };
+    //     trackConfig.compress = 0;
+    //     trackConfig.subfeatures = 1;
+    // };
+    
+    this.transformers["JBrowse/View/Track/Alignments"] = function(trackConfig) {
+        if(!trackConfig.overrideDraggable&&!browser.config.overrideDraggable) {
+            trackConfig.type = "AnnotationEditor/View/Track/DraggableAlignments";
+        }
+    };
+
+    this.transformers["JBrowse/View/Track/Alignments2"] = this.transformers["JBrowse/View/Track/Alignments"];
+
+},
+
+transform: function(trackConfig) {
+    if (trackConfig.overridePlugins||this.overridePlugins) {
+        return;
+    }
+    if (this.transformers[trackConfig.type]) {
+        var transformer = this.transformers[trackConfig.type];
+        transformer(trackConfig);
+    }
+}
+                    
+});
+
+});
---
+++ plugins/AnnotationEditor/js/main.js
@@ -0,0 +1,124 @@
+define([
+           'dojo/_base/declare',
+           'dojo/_base/array',
+           'AnnotationEditor/jslib/jquery/jquery',
+           'AnnotationEditor/FeatureEdgeMatchManager',
+           'AnnotationEditor/FeatureSelectionManager',
+           'AnnotationEditor/TrackConfigTransformer',
+           'JBrowse/Browser',
+           'JBrowse/Plugin',
+           'AnnotationEditor/View/Track/EditTrack'
+       ],
+       function(
+           declare,
+           array,
+           $,
+           FeatureEdgeMatchManager,
+           FeatureSelectionManager,
+           TrackConfigTransformer,
+           Browser,
+           JBrowsePlugin,
+           EditTrack
+       ) {
+return declare( JBrowsePlugin,
+{
+    constructor: function( args ) {
+
+        // do anything you need to initialize your plugin here
+        console.log( "plugin: AnnotationEditor" );
+        var browser = this.browser;
+        var thisB = this;
+        console.dir(browser);
+
+        // hand the browser object to the feature edge match manager
+        FeatureEdgeMatchManager.setBrowser( browser );
+
+        this.featSelectionManager = new FeatureSelectionManager();
+        this.annotSelectionManager = new FeatureSelectionManager();
+        this.trackTransformer = new TrackConfigTransformer({ browser: browser });
+
+        this.annotSelectionManager.addMutualExclusion(this.featSelectionManager);
+        this.featSelectionManager.addMutualExclusion(this.annotSelectionManager);
+
+        FeatureEdgeMatchManager.addSelectionManager(this.featSelectionManager);
+        FeatureEdgeMatchManager.addSelectionManager(this.annotSelectionManager);
+
+        array.forEach(browser.config.tracks,function(e) { thisB.trackTransformer.transform(e); })
+
+        var tracks = [
+              {
+                 "height"   : 64,
+                 "compress" : 0,
+                 "key" : "Edit",
+                 "phase" : 0,
+                 "autocomplete" : "none",
+                 "label" : "Edit",
+                 "type" : "AnnotationEditor/View/Track/EditTrack",
+                 "store" : "scratchpad",
+                 "storeClass": "AnnotationEditor/Store/SeqFeature/ScratchPad",
+                 "style" : {
+                     "className" : "transcript",
+                     "renderClassName" : "transcript-overlay",
+                     "subfeatureClasses" : {
+                         "three_prime_UTR" : "three_prime_UTR",
+                         "five_prime_UTR"  : "five_prime_UTR",
+                         "exon" : "exon",
+                         "CDS"  : "CDS",
+                         "non_canonical_splice_site" : "non-canonical-splice-site",
+                         "non_canonical_translation_start_site" : "non-canonical-translation-start-site",
+                         "non_canonical_translation_stop_site"  : "non-canonical-translation-stop-site"
+                     }
+                 },
+                 "subfeatures" : 1,
+                 "pinned" : true,
+                 "containerID":   'GenomeBrowser',
+                 "show_nav":       false,
+                 "show_overview":  false,
+                 "show_tracklist": true
+              }
+           ]
+        var config = {
+            containerID:   'GenomeBrowser',
+            show_nav:       false,
+            show_overview:  false,
+            show_tracklist: true
+        }
+        var track = {
+            "label": "Edit",
+            "type": "AnnotationEditor/View/Track/EditTrack",
+            "defaultForStoreTypes": ["AnnotationEditor/Store/SeqFeature/ScratchPad"]
+        }
+        browser.registerTrackType(track);
+        browser.registerTrackType({
+            type:                 'AnnotationEditor/View/Track/DraggableHTMLFeatures',
+            defaultForStoreTypes: [ 'JBrowse/Store/SeqFeature/NCList',
+                                    'JBrowse/Store/SeqFeature/GFF3',
+                                  ],
+            label: 'AnnotationEditor Features'
+        });
+        browser.registerTrackType({
+            type:                 'AnnotationEditor/View/Track/DraggableAlignments',
+            defaultForStoreTypes: [
+                                    'JBrowse/Store/SeqFeature/BAM'
+                                  ],
+            label: 'AnnotationEditor Alignments'
+        });
+        browser.afterMilestone('loadConfig', function () {
+            console.log("after loadConfig");
+            browser.addTracks(tracks);
+            var thigg = browser.getTrackTypes();
+            console.dir(thigg);
+        });
+
+        browser.afterMilestone('initView', function() {
+            // var gb = dojo.byId("GenomeBrowser").genomeBrowser;
+            var trackConfs = browser.trackConfigsByName["Edit"];
+            console.dir(trackConfs);
+            browser.addStoreConfig('scratchpad',trackConfs);
+
+            browser.publish( '/jbrowse/v1/c/tracks/show', trackConfs );
+            // gb.showTracks(["DNA","gene","Edit"]);
+        })
+    }
+});
+});
